7739fc2dcda1f7a86a5d79d61d13c555ef1c9606
lcagustini <torresmon235@gmail.com>
2020-01-26 20:20:20 -0300

    Initial background rendering

--- src/main.c
+++ src/main.c
@@ -31,12 +31,14 @@ PSP_HEAP_SIZE_MAX();
 #define printf	pspDebugScreenPrintf
 #endif
 
+#define ZOOM 3
+
 #ifdef PSP_BUILD
 #define SCREEN_WIDTH 480
 #define SCREEN_HEIGHT 272
 #else
-#define SCREEN_WIDTH 3*480
-#define SCREEN_HEIGHT 3*272
+#define SCREEN_WIDTH ZOOM*480
+#define SCREEN_HEIGHT ZOOM*272
 #endif
 
 SDL_Window *window;
@@ -52,28 +54,64 @@ struct input {
     int controllers_count;
 };
 
-bool initObject(int x, int y, const char *texture_path, struct object *obj) {
+enum tile_hit_type {
+    THT_NONE,
+    THT_WALL,
+};
+
+struct screen_map {
+    uint8_t tiles[60][34];
+    SDL_Texture *tile_textures[256];
+    enum tile_hit_type tile_hit_types[256];
+};
+
+SDL_Texture *loadTexture(const char *path, int *t_width, int *t_height) {
     int width, height, channels;
-    uint8_t *image = stbi_load(texture_path, &width, &height, &channels, STBI_rgb_alpha);
+    uint8_t *image = stbi_load(path, &width, &height, &channels, STBI_rgb_alpha);
     uint32_t rmask = 0x000000ff;
     uint32_t gmask = 0x0000ff00;
     uint32_t bmask = 0x00ff0000;
     uint32_t amask = 0xff000000;
 
-    SDL_Surface* loadedSurface = SDL_CreateRGBSurfaceFrom(image, width, height, STBI_rgb_alpha*8, STBI_rgb_alpha*width, rmask, gmask, bmask, amask);
+    if (t_width) *t_width = width;
+    if (t_height) *t_height = height;
 
-    if (loadedSurface) {
-        obj->texture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
+    SDL_Surface *surf = SDL_CreateRGBSurfaceFrom(image, width, height, STBI_rgb_alpha*8, STBI_rgb_alpha*width, rmask, gmask, bmask, amask);
+    if (surf) {
+        SDL_Texture *text = SDL_CreateTextureFromSurface(renderer, surf);
+        SDL_FreeSurface(surf);
+        return text;
+    }
+    return NULL;
+}
+
+bool initObject(int x, int y, const char *texture_path, struct object *obj) {
+    obj->texture = loadTexture(texture_path, &obj->rect.w, &obj->rect.h);
+    if (obj->texture) {
         obj->rect.x = x;
         obj->rect.y = y;
-        obj->rect.w = width;
-        obj->rect.h = height;
-        SDL_FreeSurface(loadedSurface);
 
         return true;
     }
 
     return false;
+}
+
+bool loadScreen(int index, struct screen_map *map) {
+    map->tile_textures[0] = loadTexture("gfx/brick.png", NULL, NULL);
+    map->tile_hit_types[0] = THT_WALL;
+
+    map->tile_textures[1] = loadTexture("gfx/wall.png", NULL, NULL);
+    map->tile_hit_types[1] = THT_NONE;
+
+    for (int y = 0; y < 34; y++) {
+        for (int x = 0; x < 60; x++) {
+            if (y % 10) map->tiles[x][y] = 1;
+            else map->tiles[x][y] = 0;
+        }
+    }
+
+    return true;
 }
 
 int main(void) {
@@ -86,9 +124,16 @@ int main(void) {
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0xFF, 0xFF);
 
+#ifndef PSP_BUILD
+    SDL_RenderSetScale(renderer, ZOOM, ZOOM);
+#endif
+
     struct input input = {0};
     input.controllers_count = SDL_NumJoysticks();
     for (int i = 0; i < input.controllers_count; i++) input.controllers[i] = SDL_JoystickOpen(i);
+
+    struct screen_map test_screen = {0};
+    loadScreen(0, &test_screen);
 
     struct object player;
     initObject(0, 0, "gfx/player.png", &player);
@@ -109,6 +154,12 @@ int main(void) {
 
         SDL_RenderClear(renderer);
 
+        for (int y = 0; y < 34; y++) {
+            for (int x = 0; x < 60; x++) {
+                SDL_Rect target = {.x = x*8, .y = y*8, .w = 8, .h = 8};
+                SDL_RenderCopy(renderer, test_screen.tile_textures[test_screen.tiles[x][y]], NULL, &target);
+            }
+        }
         SDL_RenderCopy(renderer, player.texture, NULL, &player.rect);
 
         SDL_RenderPresent(renderer);
